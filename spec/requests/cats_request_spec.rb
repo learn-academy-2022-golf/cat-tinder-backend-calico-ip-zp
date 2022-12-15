ea27147390c636dd0ad5b8049898c6ee26d78d8f
Mannyplg <pettigrew.i@gmail.com>
2022-12-15 15:50:45 -0800

    api validations completed

--- spec/requests/cats_request_spec.rb
+++ spec/requests/cats_request_spec.rb
@@ -36,6 +36,67 @@ RSpec.describe "Cats", type: :request do
 
       expect(cat.name).to eq 'Stephen'
     end
+
+    it "does not create a cat without a name" do
+      cat_params = {
+        cat:{
+          age: 13,
+          enjoys: 'digging in the trees for bird nests',
+          image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+        }
+      }
+
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['name']).to include "can't be blank"
+    end
+
+    it "does not create a cat without a age" do
+      cat_params = {
+        cat:{
+          name: 'Stephen',
+          enjoys: 'digging in the trees for bird nests',
+          image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+        }
+      }
+      
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['age']).to include "can't be blank"
+    end
+
+    it "does not create a cat without a enjoys" do
+      cat_params = {
+        cat:{
+          name: 'Stephen',
+          age: 13,
+          image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+        }
+      }
+      
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['enjoys']).to include "can't be blank"
+    end
+
+    it "does not create a cat without a image" do
+      cat_params = {
+        cat:{
+          name: 'Stephen',
+          age: 13,
+          enjoys: 'digging in the trees for bird nests',
+        }
+      }
+      
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      json = JSON.parse(response.body)
+      expect(json['image']).to include "can't be blank"
+    end
+
   end
 
   describe "PATCH /update" do
@@ -64,6 +125,106 @@ RSpec.describe "Cats", type: :request do
 
       expect(updated_cat.age).to eq 15
     end
+
+    it "it doesn't update a cat without a name" do
+      Cat.create(
+        name: 'Stephen',
+        age: 13,
+        enjoys: 'digging in the trees for bird nests',
+        image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+      )
+              cat_params = {
+          cat:{
+            name: nil,
+            age: 15,
+            enjoys: 'digging in the trees for bird nests',
+            image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+          }
+        }
+
+      cat = Cat.last
+
+      patch "/cats/#{cat.id}", params: cat_params
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body)
+
+      expect(json['name']).to include "can't be blank"
+    end
+
+    it "it doesn't update a cat without a age" do
+      Cat.create(
+        name: 'Stephen',
+        age: 13,
+        enjoys: 'digging in the trees for bird nests',
+        image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+      )
+              cat_params = {
+          cat:{
+            name: 'Stephen',
+            age: nil,
+            enjoys: 'digging in the trees for bird nests',
+            image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+          }
+        }
+
+      cat = Cat.last
+
+      patch "/cats/#{cat.id}", params: cat_params
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body)
+
+      expect(json['age']).to include "can't be blank"
+    end
+
+    it "it doesn't update a cat without a enjoys" do
+      Cat.create(
+        name: 'Stephen',
+        age: 13,
+        enjoys: 'digging in the trees for bird nests',
+        image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+      )
+              cat_params = {
+          cat:{
+            name: 'Stephen',
+            age: 15,
+            enjoys: nil,
+            image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+          }
+        }
+
+      cat = Cat.last
+
+      patch "/cats/#{cat.id}", params: cat_params
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body)
+
+      expect(json['enjoys']).to include "can't be blank"
+    end
+
+    it "it doesn't update a cat without a image" do
+      Cat.create(
+        name: 'Stephen',
+        age: 13,
+        enjoys: 'digging in the trees for bird nests',
+        image: 'https://static.onecms.io/wp-content/uploads/sites/47/2020/11/30/cat-stuck-in-tree-298823311-2000.jpg'
+      )
+              cat_params = {
+          cat:{
+            name: 'Stephen',
+            age: 15,
+            enjoys: 'digging in the trees for bird nests',
+            image: nil
+        }
+      }
+      cat = Cat.last
+
+      patch "/cats/#{cat.id}", params: cat_params
+      expect(response.status).to eq(422)
+      json = JSON.parse(response.body)
+
+      expect(json['image']).to include "can't be blank"
+    end
+
   end
   describe "DELETE /destroy" do
     it "kills a cat" do
